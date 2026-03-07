8faa661ce7d339e6b82b0f08431a04c603741e0b
christiannshuti <chrisnshuti943@gmail.com>
2026-03-07 15:44:22 +0200

    Filled enrollmentDAO

--- src/main/java/dao/EnrollmentDAO.java
+++ src/main/java/dao/EnrollmentDAO.java
@@ -11,6 +11,35 @@ public class EnrollmentDAO {
 
     public void saveEnrollment(Enrollment enrollment) {
 
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.persist(enrollment);
+        tx.commit();
+        session.close();
+    }
 
+    public List<Enrollment> getAllEnrollments() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        List<Enrollment> enrollments = session.createQuery("from Enrollment",Enrollment.class);
+        session.close();
+        return enrollments;
+    }
+
+    public Enrollment getEnrollmentById(Long id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Enrollment enrollment = session.get(Enrollment.class,id);
+        session.close();
+        return enrollment;
+    }
+
+    public void deleteEnrollment(Long id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Enrollment enrollment = session.get(Enrollment.class,id);
+        if(enrollment != null) {
+            session.remove(enrollment);
+        }
+        tx.commit();
+        session.close();
     }
 }
