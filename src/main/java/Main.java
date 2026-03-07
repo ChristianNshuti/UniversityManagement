fd9622cf5eaa37d7b06332b90ee36aff1f890a3e
christiannshuti <chrisnshuti943@gmail.com>
2026-03-07 16:39:44 +0200

    Implemented main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
 
+import model.Course;
+import model.Enrollment;
+import model.Student;
 import service.CourseService;
 import service.StudentService;
 import service.EnrollmentService;
+import model.Instructor;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +15,27 @@ public class Main {
         StudentService studentService = new StudentService();
         EnrollmentService enrollmentService = new EnrollmentService();
 
+        String studentName = "christian";
+        String studentEmail = "chrisnshuti943@gmail.com";
+        String level = "Level 5";
+        String instructorName = "Bosco";
+        String instructorEmail = "bosco@gmail.com";
+        String department = "Math";
+        Student student = new Student(studentName,studentEmail,level);
+        studentService.saveStudent(studentName,studentEmail,level);
+        Instructor instructor = new Instructor(instructorName,instructorEmail,department);
+        String title = "Mathematics";
+        courseService.saveCourse(title,instructor);
+        Course course = new Course(title,instructor);
+        enrollmentService.saveEnrollment(student,course);
+        List<Student> allStudents = studentService.getAllStudents();
+        allStudents.forEach(s -> System.out.println(s.getId() + " " + s.getName() +
+                " " + s.getEmail() + " " + s.getLevel()));
+        List<Course> allCourses = courseService.getAllCourses();
+        allCourses.forEach(c -> System.out.println(c.getId() + " " + c.getTitle() + " "
+        + c.getInstructorName() + " " + c.getEnrollments()));
+        List<Enrollment> allEnrollments = enrollmentService.getAllEnrollments();
+        allEnrollments.forEach(e -> System.out.println(e.getId() + " " + e.getStudent() + " "
+        + e.getCourse()));
     }
 }
