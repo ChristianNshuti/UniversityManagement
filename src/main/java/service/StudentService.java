f6e86dea8dc40fdce8b540f7b2a5dd2f6130279f
christiannshuti <chrisnshuti943@gmail.com>
2026-03-07 15:55:30 +0200

    Completed studentService

--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -1,3 +1,32 @@
 package service;
 
 import model.Student;
+import dao.StudentDAO;
+
+import java.util.*;
+
+public class StudentService {
+
+    private final StudentDAO studentDAO = new StudentDAO();
+
+    public void saveStudent(String name,String email,String level) {
+        Student student = new Student(name,email,level);
+        studentDAO.saveStudent(student);
+    }
+
+    public Student getStudentById(Long id) {
+        Student student = studentDAO.getStudentById(id);
+        return student;
+    }
+
+    public List<Student> getAllStudents() {
+        List<Student> students = studentDAO.getAllStudents();
+        return students;
+    }
+
+    public void deleteStudent(Long id) {
+        studentDAO.deleteStudent(id);
+    }
+
+
+}
