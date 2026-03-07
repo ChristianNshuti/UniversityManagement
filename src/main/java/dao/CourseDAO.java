16fc8c85eef3158d04b23dcd64c5cbd17aed8d39
christiannshuti <chrisnshuti943@gmail.com>
2026-03-07 14:09:04 +0200

    Save enrollment method for enrollmentDAO

---
+++ src/main/java/dao/CourseDAO.java
@@ -0,0 +1,58 @@
+package dao;
+
+import model.Course;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtil;
+
+import java.util.List;
+
+public class CourseDAO {
+
+    public void saveCourse(Course course) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        session.persist(course);
+
+        tx.commit();
+        session.close();
+    }
+
+    public Course getCourseById(Long id) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        Course course = session.get(Course.class,id);
+
+        session.close();
+        return course;
+    }
+
+    public List<Course> getAllCourses() {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        List<Course> courses = session.createQuery("from Course",Course.class).list();
+
+        session.close();
+        return courses;
+    }
+
+    public void deleteCourse(Long id) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        Course course = session.get(Course.class,id);
+        if(course != null) {
+            session.remove(course);
+        }
+
+        tx.commit();
+        session.close();
+    }
+
+
+}
---
+++ src/main/java/dao/EnrollmentDAO.java
@@ -0,0 +1,16 @@
+package dao;
+
+import model.Enrollment;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtil;
+
+import java.util.List;
+
+public class EnrollmentDAO {
+
+    public void saveEnrollment(Enrollment enrollment) {
+
+
+    }
+}
